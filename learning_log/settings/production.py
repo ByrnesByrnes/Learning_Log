b46779b911dff0b47337f23e03fd9dbdde369f2f
Myron Byrnes <myronbyrnes@gmail.com>
2018-09-17 03:55:52 -0400

    Host ALLOWED Update

--- learning_log/settings/production.py
+++ learning_log/settings/production.py
@@ -26,7 +26,7 @@ SECRET_KEY = ')kniih(eqg)^=*ctd7%!x=ij_x7q@3@=%_krwd+4-u^l%cdw10'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 LOGIN_URL = '/login/'
 # Application definition
